41108b05784df2b62379cd3a876f9b88d1702de2
Jeruk99 <vlad_ladzd@mail.ru>
2022-03-21 19:57:29 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    double x, y;
+    cout << "Введите x" << endl;
+    cin >> x;
+    y = (atan(1 + pow(sin(x), 2)) + log2(1 + exp(-x))) / (sqrt(sqrt(1 + pow(cos(x), 2))));
+    cout << "y(x)" << y << endl;
+    system("pause");
+    return 0;
+
+}
